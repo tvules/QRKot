cb998bb28e4d8c2cdd415d842b1042c86da91e20
Ilya Petrukhin <tvules23@yandex.ru>
2023-02-02 21:01:55 +0400

    [features] Add routes for User management

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.api.routers import main_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_title, description=settings.description)
+
+app.include_router(main_router)
